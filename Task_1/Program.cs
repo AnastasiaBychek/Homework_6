f1540db1b5f3f9c8b2a7637d2c769fa6462ca6a8
Anastasia Bychek <nastya900063@gmail.com>
2022-07-10 20:20:07 +0300

    Homework 6 task added

---
+++ Task_1/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine ("Enter required number of elements");
+int M = Convert.ToInt32(Console.ReadLine());
+int [] FillArray()
+{
+    int [] array = new int [M];
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Enter element");
+        int element = Convert.ToInt32(Console.ReadLine());
+        array[i] = element;
+    }
+    return array;
+}
+
+int[] arr = FillArray();
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]} ");
+    }
+}
+ShowArray(arr);
+
+void CountElements(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if( array[i]>0)
+        {
+            count = count +1;
+        }
+    }
+     Console.WriteLine($"There are  {count} greater then 0 elements in array");
+}
+CountElements(arr);
---
+++ Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter k1");
+ int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter b1");
+ int b1 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Enter k2");
+ int k2 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Enter b2");
+ int b2 = Convert.ToInt32(Console.ReadLine());
+
+ void Method (int k1, int b1, int k2, int b2)
+ {
+    double x = (b2 - b1)/(k2-k1);
+    double y = k1*((b2 - b1)/(k2-k1))+b1;
+    Console.WriteLine($"{x}, {y}");
+ }
+
+Method(k1,b1, k2, b2);
